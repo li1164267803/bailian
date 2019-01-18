ac95486ce5cb55807e52da904b97dbf38c7a8888
a <a>
2019-01-18 08:54:21 +0800

    第二次提交

--- xiangmu/js/index.js
+++ xiangmu/js/index.js
@@ -1,4 +1,15 @@
 window.onload = function(){
+	//吸顶效果
+	$(window).scroll(function(){
+		if( $(".fileheader").offset().top > 200 ){
+			$(".fileheader").slideDown(200)
+			console.log(2)
+		}else if( $(".fileheader").offset().top < 100 ){
+			$(".fileheader").slideUp(200)
+		}
+		console.log(  )
+//		console.log(1)
+	})
 //	顶部左边划上小箭头
 	$(".bl-left").hover(function(){
 		$(".gang").html("&nbsp;");
@@ -14,17 +25,26 @@ window.onload = function(){
 		$(this).find("i").removeClass().addClass("test-normal")
 	})
 	
-//	轮播图
+//	banner轮播图
 	var ulist = $(".banner-wrap ul").find("li");
 	var olist = $(".banner-wrap ol").find("li");
 	var timer = null;
 	var index = 0;
-	timer = setInterval(function(){
+	timer = setInterval(autoplay,2000);
+	function autoplay(){
 		index++;
 		if(index == olist.length){
 			index = 0
 		}
 		ulist.eq(index).fadeIn(200).siblings().fadeOut();
 		olist.eq(index).addClass("current").siblings().removeClass()
-	},2000)
+	};
+	olist.hover(function(){
+		clearInterval(timer);
+		var index = $(this).index();
+		ulist.eq(index).fadeIn(200).siblings().fadeOut();
+		olist.eq(index).addClass("current").siblings().removeClass()
+	},function(){
+		timer = setInterval(autoplay,2000);
+	})
 }
